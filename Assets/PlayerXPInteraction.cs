e32d55c5324123ca81b27ed470fc403169455832
romerocarmen <carmen.clarissa.romero@gmail.com>
2023-07-27 15:24:57 -0700

    created the XP object & made it so that when the player comes into contact with the XP-- it disappears

---
+++ Assets/PlayerXPInteraction.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerXPInteraction : MonoBehaviour
+{
+    // tracking the number of XP collected
+    public static int totalXP = 0; 
+
+    void Awake()
+    {
+        //Make Collider2D as trigger 
+        GetComponent<Collider2D>().isTrigger = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        // XP disappears if it comes into contact with player
+        if (collider.CompareTag("Player"))
+        {
+            // add XP to counter
+            totalXP++;
+            // Destroy the XP
+            Destroy(gameObject);
+        }
+    }
+}
