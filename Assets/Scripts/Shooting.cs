31b26748cbcba672aa0362a628ebb8ba5e2b3cdf
0nlysykes <jpelliottdev@gmail.com>
2023-07-31 20:20:24 -0700

    player go spin now

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -6,19 +6,23 @@ public class Shooting : MonoBehaviour
 {
     [SerializeField]
     private GameObject Bullet;
+    private float timeSinceLastBullet = 1;
+    public float fireRate;
     //public float fireRate = 1f;
     //public float canFire = 1f;
 
     void Start()
     {
-
+        
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) //&& Time.time > canFire
+        timeSinceLastBullet += Time.deltaTime;
+        if (Input.GetMouseButton(0) && timeSinceLastBullet > 1/fireRate) //&& Time.time > canFire
         {
            Instantiate(Bullet, transform.position, transform.rotation);
+           timeSinceLastBullet = 0;
            //canFire = Time.time + fireRate;
         }
     }
