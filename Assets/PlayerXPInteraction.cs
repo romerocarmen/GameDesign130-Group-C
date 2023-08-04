d94eb5b50b9384989b8112d871ce55f18ed2ddfa
romerocarmen <carmen.clarissa.romero@gmail.com>
2023-08-04 16:52:18 -0700

    added smile

--- Assets/PlayerXPInteraction.cs
+++ Assets/PlayerXPInteraction.cs
@@ -15,7 +15,7 @@ public class PlayerXPInteraction : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collider)
     {
-        // XP disappears if it comes into contact with player object
+        // XP disappears if it comes into contact with player object :)
         if (collider.CompareTag("Player"))
         {
             // Finds the audio source
