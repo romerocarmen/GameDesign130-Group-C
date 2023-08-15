52eb34adecff2553876719919f274a61f6c474aa
Cai Spidel <chiroptera.cai@gmail.com>
2023-08-14 19:33:19 -0700

    Improve UI, Improve safe zone movement

--- Assets/Scripts/RandomMovement.cs
+++ Assets/Scripts/RandomMovement.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class RandomMovement : MonoBehaviour
 {
     private Rigidbody2D rb2d;
-    [SerializeField] private float speed = 15f;
-    [SerializeField] private float lowerSpeed = 5f;
+    //[SerializeField] private float speed = 15f;
+    //[SerializeField] private float lowerSpeed = 5f;
+
+    [SerializeField] public float maxSpeed = 5f;
+    [SerializeField] public float speedAcc = .05f;
+    [SerializeField] private float currentSpeed; 
     // Use this for initialization
     void Start()
     {
@@ -15,15 +19,43 @@ public class RandomMovement : MonoBehaviour
 
         rb2d = this.GetComponent<Rigidbody2D>();
 
-        float aSpeed = Random.Range(speed, lowerSpeed);
+        //float aSpeed = Random.Range(speed, lowerSpeed);
 
-        rb2d.AddForce(transform.right * aSpeed);
+        //rb2d.AddForce(transform.right * aSpeed);
+        StartCoroutine(speedIncrease());
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        currentSpeed = rb2d.velocity.magnitude; 
         
     }
+
+    void LateUpdate()
+    {
+        Vector3 worldDirectionToPointForward = rb2d.velocity.normalized;
+        Vector3 localDirectionToPointForward = Vector3.right;
+
+        Vector3 currentWorldForwardDirection = transform.TransformDirection(
+                localDirectionToPointForward);
+        float angleDiff = Vector3.SignedAngle(currentWorldForwardDirection,
+                worldDirectionToPointForward, Vector3.forward);
+
+        transform.Rotate(Vector3.forward, angleDiff, Space.World);
+    }
+
+    IEnumerator speedIncrease()
+    {
+        
+        if (currentSpeed < maxSpeed)
+        {
+
+            rb2d.AddForce(transform.right * speedAcc * Time.deltaTime);
+        }
+        
+        yield return null;
+        StartCoroutine(speedIncrease());
+    }
+
 }
