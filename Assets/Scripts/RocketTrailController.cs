a20faa5e6e354ce1f1c859cd4177535bf8e0d52a
Cai Spidel <chiroptera.cai@gmail.com>
2023-08-26 11:17:15 -0700

    Added Sound Effect, Controls Scene, More Lighting, Fixed Misc Bugs

--- Assets/Scripts/RocketTrailController.cs
+++ Assets/Scripts/RocketTrailController.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class RocketTrailController : MonoBehaviour
 {
+    [SerializeField] public AudioClip theClip;
+    [SerializeField] private AudioSource boosterAudio;
+    [SerializeField] private float volume = 1f;
+
+    [SerializeField] private bool isPlaying;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isPlaying = false; 
     }
 
     // Update is called once per frame
@@ -23,6 +29,16 @@ public class RocketTrailController : MonoBehaviour
         {
             foreach (Transform child in transform)
                 child.gameObject.SetActive(false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            boosterAudio.Play();
+            isPlaying= true;
+        } 
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            boosterAudio.Stop();
+            isPlaying= false;   
         }
     }
 }
