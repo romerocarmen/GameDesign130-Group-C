3957f0ecd351ae87259c2732a166f75af40f99ef
David <armonddavid@gmail.com>
2023-08-30 05:16:22 +0800

    Music persists across scenes

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("-----Audio Source-----")]
+    [SerializeField] AudioSource musicSource;
+
+    [Header("-----Audio Clip-----")]
+    public AudioClip mainMenu;
+
+    public static AudioManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        musicSource.clip = mainMenu;
+        musicSource.Play();
+    }
+
+    void Update()
+    {
+        if (Application.loadedLevelName == "SampleScene")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
